959a1d8e83c98fc2a4b8e2cd99d61256cff6976e
Juarez de Pinho <juarezpinho20s@outlook.com>
2024-05-23 02:02:25 -0300

    criando views.py

--- gerenciador/views.py
+++ gerenciador/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Usuario, Item, Pedido
+from .serializers import UsuarioSerializer, ItemSerializer, PedidoSerializer
 
-# Create your views here.
+class UsuarioViewSet(viewsets.ModelViewSet):
+    queryset = Usuario.objects.all()
+    serializer_class = UsuarioSerializer
+
+class ItemViewSet(viewsets.ModelViewSet):
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
+
+class PedidoViewSet(viewsets.ModelViewSet):
+    queryset = Pedido.objects.all()
+    serializer_class = PedidoSerializer
