3ab1a224f8b396abd1bafdca783607708edec488
Juarez de Pinho <juarezpinho20s@outlook.com>
2024-05-23 02:01:08 -0300

    criando serializers.py

---
+++ gerenciador/serializers.py
@@ -0,0 +1,17 @@
+from rest_framework import serializers
+from .models import Usuario, Item, Pedido
+
+class UsuarioSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Usuario
+        fields = '__all__'
+
+class ItemSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Item
+        fields = '__all__'
+
+class PedidoSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Pedido
+        fields = '__all__'
