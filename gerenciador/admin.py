d0b48a639d2e2daa229404c527275be7957ff457
Juarez de Pinho <juarezpinho20s@outlook.com>
2024-05-23 01:55:39 -0300

    migrate inicial

--- gerenciador/admin.py
+++ gerenciador/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from .models import Usuario, Item, Pedido
 
-# Register your models here.
+class UsuarioAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'email', 'cpf', 'celular')
+
+class ItemAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'codigo_item', 'preco')
+
+class PedidoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'usuario', 'data_pedido')
+
+admin.site.register(Usuario, UsuarioAdmin)
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Pedido, PedidoAdmin)
